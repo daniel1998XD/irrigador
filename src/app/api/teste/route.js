a23e1374ab78727bb6d85e7883020c1406ef7a7b
Daniel <daniel12.souza98@hotmail.com>
2025-06-23 16:32:55 -0300

    testando conectar com banco

--- src/app/api/teste/route.js
+++ src/app/api/teste/route.js
@@ -1,5 +1,14 @@
 import { NextResponse } from 'next/server';
+import dbConnect from '@/lib/dbConnect';
 
 export async function GET() {
-  return NextResponse.json({ message: "A API está no ar e a rota funciona!" });
+  try {
+    await dbConnect();
+    return NextResponse.json({ status: "sucesso", message: "Conexão com MongoDB bem-sucedida!" });
+  } catch (error) {
+    return NextResponse.json(
+      { status: "erro", message: "Falha ao conectar com MongoDB", error: error.message },
+      { status: 500 }
+    );
+  }
 }
