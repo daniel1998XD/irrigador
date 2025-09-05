46302d61461dc734f8e32b8670373ef9b31a993b
daniel1998XD <daniel12.souza98@hotmail.com>
2025-09-05 09:59:45 -0300

    Update userState.js

--- src/models/userState.js
+++ src/models/userState.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const userStateSchema = new mongoose.Schema({
   chatId: { type: String, required: true, unique: true },
-  action: { type: String, required: true }, // ex: 'awaiting_new_name'
+  action: { type: String, required: true }, 
   data: { type: mongoose.Schema.Types.Mixed }, // Para armazenar dados contextuais, como o profileId
   updatedAt: { type: Date, default: Date.now, expires: '5m' } // O estado expira em 5 minutos
 });
