4563363e33ab2f0557f8c8700e63ba4c66e9a74a
daniel1998XD <daniel12.souza98@hotmail.com>
2025-09-05 09:41:20 -0300

    Update deviceStatus.js

--- src/models/deviceStatus.js
+++ src/models/deviceStatus.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 const deviceStatusSchema = new mongoose.Schema({
-  // Usaremos um identificador fixo para saber que este é o documento do nosso único dispositivo.
   identifier: { 
     type: String, 
     default: 'main_device', // Nome padrão para nosso dispositivo principal
